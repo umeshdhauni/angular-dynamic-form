ff50cc954a98fb15b3eb775cd402c0b08956aa54
Umesh Dhauni <umeshdhauni@Umeshs-MacBook-Air.local>
2019-06-08 21:59:22 +0530

    done for second day

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormArray, Validators, FormControl, AbstractControl } from
 import { Observable } from 'rxjs';
 
 function positiveNumber(control: AbstractControl): { [key: string]: boolean } | null {
-  console.log(control.value)
+  // console.log(control.value)
 
   if ((String(control.value) == '' || String(control.value) == 'null')) {
     return null;
@@ -11,8 +11,22 @@ function positiveNumber(control: AbstractControl): { [key: string]: boolean } | 
   else if ((isNaN(control.value))) {
     return { 'nan': true };
   }
-  else if ((control.value < 10 || control.value > 1000 )) {
+  else if ((control.value < 10 || control.value > 1000)) {
     return { 'notvalid': true }
+  }
+
+  return null;
+}
+
+function limitNumber(control: AbstractControl): { [key: string]: boolean } | null {
+  if ((String(control.value) == '' || String(control.value) == 'null')) {
+    return null;
+  }
+  else if(isNaN(control.value)){
+    control.patchValue('');
+  }
+  else if (control.value.length != 4) {
+    return { 'notvalid': true };
   }
 
   return null;
@@ -25,90 +39,69 @@ function positiveNumber(control: AbstractControl): { [key: string]: boolean } | 
 })
 export class FormComponent implements OnInit {
   form;
-  checkValid=[{amount:0,valid:false}]
+  checkValid = [{ count: 0, subtotal: 0, amount: 0 }]
   // amountValid= false;
-  subTotal = [];
+  totalArray = [];
   netTotal = 0;
   formChanges: Observable<any>
-  singleEntity = [
+  checkboxes = [
     {
-      amount: {
-        name: 'Amount 1',
-        key: 'amount1',
-        disable: true
-      },
-      currency: {
-        name: 'C1',
-        key: 'c1',
-        checked: false
-      }
+      label: 'C1',
+      checked: false,
+      key: 'c1'
     },
     {
-      amount: {
-        name: 'Amount 2',
-        key: 'amount2',
-        disable: true
+      label: 'C2',
+      checked: false,
+      key: 'c2'
 
-      },
-      currency: {
-        name: 'C2',
-        key: 'c2',
-        checked: false
-      }
     },
     {
-      amount: {
-        name: 'Amount 3',
-        key: 'amount3',
-        disable: true
+      label: 'C3',
+      checked: false,
+      key: 'c3'
 
-      },
-      currency: {
-        name: 'C3',
-        key: 'c3',
-        checked: false
-      }
     },
     {
-      amount: {
-        name: 'Amount 4',
-        key: 'amount4',
-        disable: true
+      label: 'C4',
+      checked: false,
+      key: 'c4'
 
-      },
-      currency: {
-        name: 'C4',
-        key: 'c4',
-        checked: false
-      }
     },
     {
-      amount: {
-        name: 'Amount 5',
-        key: 'amount5',
-        disable: true
+      label: 'C5',
+      checked: false,
+      key: 'c5'
 
-      },
-      currency: {
-        name: 'C5',
-        key: 'c5',
-        checked: false
-      }
     },
     {
-      amount: {
-        name: 'Amount 6',
-        key: 'amount6',
-        disable: true
+      label: 'C6',
+      checked: false,
+      key: 'c6'
 
-      },
-      currency: {
-        name: 'C6',
-        key: 'c6',
-        checked: false
-      }
+    }
+  ];
+  amounts = [
+    {
+      label: 'Amount 1',
+      key: 'amount1',
+      input: false
     },
-
+    {
+      label: 'Amount 2',
+      key: 'amount2',
+      input: false
+    },
+    {
+      label: 'Amount 3',
+      key: 'amount3',
+      input: false
+    },
+    {
+      label: 'Amount 4',
+      key: 'amount4',
+      input: false
+    }
   ]
   constructor(
     private fb: FormBuilder
@@ -127,65 +120,95 @@ export class FormComponent implements OnInit {
           amount2: ['', positiveNumber],
           amount3: ['', positiveNumber],
           amount4: ['', positiveNumber],
-          amount5: ['', positiveNumber],
-          amount6: ['', positiveNumber],
           c1: [''],
           c2: [''],
           c3: [''],
           c4: [''],
           c5: [''],
           c6: [''],
-          subTotal: ['']
+          box: ['',limitNumber]
         })
       ])
     })
 
     this.formChanges = this.form.valueChanges;
 
+    // this.formChanges.subscribe(res => {
+    //   if (res) {
+    //     this.subTotal = [];
+    //     res.entity.forEach((element, index) => {
+    //       let subtotal = 0;
+    //       let count = 0;
+    //       this.singleEntity.forEach((item, i) => {
+    //         if (element[item.currency.key] == true) {
+    //           count++;
+    //           this.singleEntity[i].currency.checked = true;
+    //           subtotal = subtotal + element[item.amount.key];
+    //         }
+    //         else {
+    //           this.singleEntity[i].currency.checked = false;
+    //         }
+    //       })
+
+    //       let checked = this.singleEntity.filter(item =>{
+    //         return (item.currency.checked == true && element[item.amount.key]>0);
+    //       })
+    //       let average = subtotal/count?subtotal/count:0
+    //       this.subTotal[index] = {
+    //         amount:average,
+    //         valid:checked.length>0?true:false
+    //       };
+
+    //       this.netTotal = this.checkTotal('amount');
+    //     });
+
+    //     this.checkValid = this.subTotal.filter(one =>{
+    //       return one.valid == false;
+    //     })
+    //   }
+
+
+    // })
+
+
     this.formChanges.subscribe(res => {
       if (res) {
-        // console.log(res);
-        this.subTotal = [];
+        // console.log(res)
         res.entity.forEach((element, index) => {
-          let subtotal = 0;
           let count = 0;
-          this.singleEntity.forEach((item, i) => {
-            if (element[item.currency.key] == true) {
+          let subtotal = 0;
+          let amount = 0;
+          for (let key in element) {
+            if (element[key] == true) {
               count++;
-              this.singleEntity[i].currency.checked = true;
-              subtotal = subtotal + element[item.amount.key];
             }
-            else {
-              this.singleEntity[i].currency.checked = false;
-            }
-          })
-
-          let checked = this.singleEntity.filter(item =>{
-            return (item.currency.checked == true && element[item.amount.key]>0);
-          })
-          let average = subtotal/count?subtotal/count:0
-          this.subTotal[index] = {
-            amount:average,
-            valid:checked.length>0?true:false
-          };
-
-          this.netTotal = this.checkTotal('amount');
-          // this.netTotal = this.subTotal.reduce((a, b) => a.amount + b.amount, 0) ? this.subTotal.reduce((a, b) => a.amount + b.amount, 0) : 0
+          }
+          amount = (element.amount1 ? element.amount1 : 0) + (element.amount2 ? element.amount2 : 0) + (element.amount3 ? element.amount3 : 0) + (element.amount4 ? element.amount4 : 0)
+          subtotal = count?(amount) / count:0;
+          this.totalArray[index] = {
+            amount: amount,
+            count: count,
+            subTotal: subtotal ? subtotal : 0
+          }
         });
 
-        this.checkValid = this.subTotal.filter(one =>{
-          return one.valid == false;
-        })
-      }
-      
+        console.log(this.totalArray)
 
+        this.checkTotal(this.totalArray);
+      }
+    });
+
+  }
+
+  checkTotal(data) {
+    this.netTotal =  data.reduce((a, b) => a + (b['subTotal'] || 0), 0);
+    this.checkValid = data.filter(item => {
+      return (item.amount == 0 || item.count == 0)
     })
 
+    // console.log(this.netTotal,this.checkValid)
   }
 
-  checkTotal(key){
-    return this.subTotal.reduce((a, b) => a + (b[key] || 0), 0);
-  }
 
   addEntity() {
     let control = <FormArray>this.form.controls.entity;
@@ -195,15 +218,13 @@ export class FormComponent implements OnInit {
         amount2: ['', positiveNumber],
         amount3: ['', positiveNumber],
         amount4: ['', positiveNumber],
-        amount5: ['', positiveNumber],
-        amount6: ['', positiveNumber],
         c1: [''],
         c2: [''],
         c3: [''],
         c4: [''],
         c5: [''],
         c6: [''],
-        subTotal: ['']
+        box: ['',limitNumber]
       })
     );
   }
@@ -213,9 +234,9 @@ export class FormComponent implements OnInit {
   }
 
 
-  submit(data){
+  submit(data) {
     console.log(data);
-    console.log(this.subTotal,'data');
+    console.log(this.totalArray, 'data');
   }
 
 }
