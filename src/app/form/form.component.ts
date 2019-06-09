710c6ab46182c93346a0b8b5ee4c825bb7b601d6
Umesh Dhauni <umeshdhauni@Umeshs-MacBook-Air.local>
2019-06-09 13:53:57 +0530

    done for 3 day

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -20,7 +20,7 @@ function positiveNumber(control: AbstractControl): { [key: string]: boolean } | 
 
 function limitNumber(control: AbstractControl): { [key: string]: boolean } | null {
   if ((String(control.value) == '' || String(control.value) == 'null')) {
-    return null;
+    return { 'require': true };
   }
   else if(isNaN(control.value)){
     control.patchValue('');
@@ -41,7 +41,6 @@ export class FormComponent implements OnInit {
   form;
   checkValid = [{ count: 0, subtotal: 0, amount: 0 }]
   // amountValid= false;
-  totalArray = [];
   netTotal = 0;
   formChanges: Observable<any>
   checkboxes = [
@@ -126,87 +125,46 @@ export class FormComponent implements OnInit {
           c4: [''],
           c5: [''],
           c6: [''],
-          box: ['',limitNumber]
+          box: ['',limitNumber],
+          subTotal:['']
         })
       ])
     })
 
     this.formChanges = this.form.valueChanges;
 
-    // this.formChanges.subscribe(res => {
-    //   if (res) {
-    //     this.subTotal = [];
-    //     res.entity.forEach((element, index) => {
-    //       let subtotal = 0;
-    //       let count = 0;
-    //       this.singleEntity.forEach((item, i) => {
-    //         if (element[item.currency.key] == true) {
-    //           count++;
-    //           this.singleEntity[i].currency.checked = true;
-    //           subtotal = subtotal + element[item.amount.key];
-    //         }
-    //         else {
-    //           this.singleEntity[i].currency.checked = false;
-    //         }
-    //       })
-
-    //       let checked = this.singleEntity.filter(item =>{
-    //         return (item.currency.checked == true && element[item.amount.key]>0);
-    //       })
-    //       let average = subtotal/count?subtotal/count:0
-    //       this.subTotal[index] = {
-    //         amount:average,
-    //         valid:checked.length>0?true:false
-    //       };
-
-    //       this.netTotal = this.checkTotal('amount');
-    //     });
-
-    //     this.checkValid = this.subTotal.filter(one =>{
-    //       return one.valid == false;
-    //     })
-    //   }
-
-
-    // })
-
 
     this.formChanges.subscribe(res => {
       if (res) {
-        // console.log(res)
         res.entity.forEach((element, index) => {
           let count = 0;
           let subtotal = 0;
           let amount = 0;
           for (let key in element) {
-            if (element[key] == true) {
-              count++;
+            if(key != 'box'){
+              if (element[key] == true) {
+                console.log(key)
+                count++;
+              }
             }
           }
           amount = (element.amount1 ? element.amount1 : 0) + (element.amount2 ? element.amount2 : 0) + (element.amount3 ? element.amount3 : 0) + (element.amount4 ? element.amount4 : 0)
-          subtotal = count?(amount) / count:0;
-          this.totalArray[index] = {
-            amount: amount,
-            count: count,
-            subTotal: subtotal ? subtotal : 0
-          }
+          subtotal = count?(amount) * count:0;
+          this.form.controls['entity']['controls'][index]['controls']['subTotal'].setValue(subtotal,{emitEvent: false});
         });
 
-        console.log(this.totalArray)
-
-        this.checkTotal(this.totalArray);
+        this.checkTotal(this.form.value.entity);
       }
     });
 
   }
 
   checkTotal(data) {
+    console.log(data);
     this.netTotal =  data.reduce((a, b) => a + (b['subTotal'] || 0), 0);
     this.checkValid = data.filter(item => {
-      return (item.amount == 0 || item.count == 0)
+      return (item.subTotal == 0)
     })
-
-    // console.log(this.netTotal,this.checkValid)
   }
 
 
@@ -224,7 +182,8 @@ export class FormComponent implements OnInit {
         c4: [''],
         c5: [''],
         c6: [''],
-        box: ['',limitNumber]
+        box: ['',limitNumber],
+        subTotal:['']
       })
     );
   }
@@ -236,7 +195,6 @@ export class FormComponent implements OnInit {
 
   submit(data) {
     console.log(data);
-    console.log(this.totalArray, 'data');
   }
 
 }
